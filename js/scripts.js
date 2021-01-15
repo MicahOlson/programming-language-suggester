fb79bbed9c2ac1cf29724e067cba6e8867abe405
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-01-15 10:13:19 -0800

    Fix debug errors for user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
+  console.log("Script executing!")
   $("#questionnaire").submit(function(event) {
     console.log("Submit form successfully reached.")
     const name = $("input#name").val();
-    const sign = $("input#zodiac").val();
+    const sign = $("select#zodiac").val();
     const animal = $("input:radio[name=animal]:checked").val();
     const country = $("input:radio[name=country]:checked").val();
     const year = $("input:radio[name=year]:checked").val();
@@ -16,6 +17,8 @@ $(document).ready(function() {
     console.log("Year: " + year);
     console.log("Color: " + color);
     console.log("Musketeer: " + musketeer);
+
+    $("#result").text("We recommend you learn Python!");
     
     event.preventDefault();
   });
