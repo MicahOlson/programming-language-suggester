da536792d3ed048a1d29e6ceece03e1b4eb290ea
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-01-15 10:04:17 -0800

    Give all forms unique html ids and outline js user interface logic

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("#questionnaire").submit(function(event) {
+    console.log("Submit form successfully reached.")
+    const name = $("input#name").val();
+    const sign = $("input#zodiac").val();
+    const animal = $("input:radio[name=animal]:checked").val();
+    const country = $("input:radio[name=country]:checked").val();
+    const year = $("input:radio[name=year]:checked").val();
+    const color = $("input:radio[name=color]:checked").val();
+    const musketeer = $("input:radio[name=musketeer]:checked").val();
+    
+    console.log("Name: " + name);
+    console.log("Sign: " + sign);
+    console.log("Animal: " + animal);
+    console.log("Country: " + country);
+    console.log("Year: " + year);
+    console.log("Color: " + color);
+    console.log("Musketeer: " + musketeer);
+    
+    event.preventDefault();
+  });
+});
